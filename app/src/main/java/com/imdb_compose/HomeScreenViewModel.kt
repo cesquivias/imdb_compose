e57a7f77703c65d18a9133b782007263168bcbc8
Alberto Esquivias <alberto.m.esquivias@gmail.com>
2024-10-02 21:16:05 -0700

    adds box office box

--- app/src/main/java/com/imdb_compose/HomeScreenViewModel.kt
+++ app/src/main/java/com/imdb_compose/HomeScreenViewModel.kt
@@ -48,6 +48,7 @@ class HomeScreenViewModel() : ViewModel() {
     val peopleApi = Retrofit.getInstance().create(PeopleApi::class.java)
 
     val catagories: List<String> = listOf(
+        "Top box office",
         "Popular actors",
         "Trending tv",
         "Tv airing today",
@@ -57,6 +58,19 @@ class HomeScreenViewModel() : ViewModel() {
     )
 
     val noMovies: MutableStateFlow<MovieList?> = MutableStateFlow(null)
+
+    val boxOffice = listOf(
+        mapOf("Average" to "$7,968", "Change" to "-403", "Distributor" to "Warner Bros.", "Gross" to "$33,244,516", "LW" to "1", "Rank" to "1", "Release" to "Beetlejuice Beetlejuice", "Response" to "False", "Theaters" to "4,172", "Total Gross" to "$234,092,898", "Weeks" to "3", "date range" to "September 16 - September 22"),
+        mapOf("Average" to "$7,507", "Change" to "-", "Distributor" to "Paramount Pictures", "Gross" to "$29,864,458", "LW" to "-", "Rank" to "2", "Release" to "Transformers One", "Response" to "False", "Theaters" to "3,978", "Total Gross" to "$29,864,458", "Weeks" to "1", "date range" to "September 16 - September 22"),
+        mapOf("Average" to "$2,454", "Change" to "-", "Distributor" to "Universal Pictures", "Gross" to "$8,285,530", "LW" to "2", "Rank" to "3", "Release" to "Speak No Evil", "Response" to "False", "Theaters" to "3,375", "Total Gross" to "$23,840,120", "Weeks" to "2", "date range" to "September 16 - September 22"),
+        mapOf("Average" to "$2,269", "Change" to "-", "Distributor" to "Lionsgate Films", "Gross" to "$6,052,523", "LW" to "-", "Rank" to "4", "Release" to "Never Let Go", "Response" to "False", "Theaters" to "2,667", "Total Gross" to "$6,052,523", "Weeks" to "1", "date range" to "September 16 - September 22"),
+        mapOf("Average" to "$2,128", "Change" to "-625", "Distributor" to "Walt Disney Studios Motion Pictures", "Gross" to "$5,215,484", "LW" to "3", "Rank" to "5", "Release" to "Deadpool & Wolverine", "Response" to "False", "Theaters" to "2,450", "Total Gross" to "$628,600,109", "Weeks" to "9", "date range" to "September 16 - September 22"),
+        mapOf("Average" to "$2,595", "Change" to "-", "Distributor" to "MUBI", "Gross" to "$5,058,960", "LW" to "-", "Rank" to "6", "Release" to "The Substance", "Response" to "False", "Theaters" to "1,949", "Total Gross" to "$5,058,960", "Weeks" to "1", "date range" to "September 16 - September 22"),
+        mapOf("Average" to "$2,181", "Change" to "+83", "Distributor" to "SDG Releasing", "Gross" to "$3,489,975", "LW" to "4", "Rank" to "7", "Release" to "Am I Racist?", "Response" to "False", "Theaters" to "1,600", "Total Gross" to "$9,961,482", "Weeks" to "2", "date range" to "September 16 - September 22"),
+        mapOf("Average" to "$1,410", "Change" to "-600", "Distributor" to "-", "Gross" to "$2,609,564", "LW" to "5", "Rank" to "8", "Release" to "Reagan", "Response" to "False", "Theaters" to "1,850", "Total Gross" to "$27,470,165", "Weeks" to "4", "date range" to "September 16 - September 22"),
+        mapOf("Average" to "$1,396", "Change" to "-600", "Distributor" to "Walt Disney Studios Motion Pictures", "Gross" to "$1,885,189", "LW" to "7", "Rank" to "9", "Release" to "Alien to Romulus", "Response" to "False", "Theaters" to "1,350", "Total Gross" to "$104,180,339", "Weeks" to "6", "date range" to "September 16 - September 22"),
+        mapOf("Average" to "$1,511", "Change" to "-433", "Distributor" to "Affirm Films", "Gross" to "$1,785,380", "LW" to "9", "Rank" to "10", "Release" to "The Forge", "Response" to "False", "Theaters" to "1,181", "Total Gross" to "$26,856,127", "Weeks" to "5", "date range" to "September 16 - September 22")
+    )
 
     // Movies
     private val _movieListOfWeek: MutableStateFlow<MovieList?> = MutableStateFlow(null)
@@ -401,25 +415,3 @@ data class ActorDetail(
     val profile_path: String,
     val popularity: String
 )
-/**
- * {
- *   "adult": false,
- *   "also_known_as": [
- *     "James Earl Boggins Jones",
- *     "Todd Jones",
- *     "Jimmy"
- *   ],
- *   "biography": "James Earl Jones (January 17, 1931 – September 9, 2024) was an American actor. He was described as \"one of America's most distinguished and versatile\" actors for his performances on stage and screen, and \"one of the greatest actors in American history\". Over his career, he received three Tony Awards, two Emmy Awards, and a Grammy Award. He was inducted into the American Theater Hall of Fame in 1985. He was honored with the National Medal of Arts in 1992, the Kennedy Center Honor in 2002, the Screen Actors Guild Life Achievement Award in 2009 and the Honorary Academy Award in 2011. His deep voice has been praised as a \"stirring basso profondo that has lent gravel and gravitas\" to his projects.",
- *   "birthday": "1931-01-17",
- *   "deathday": "2024-09-09",
- *   "gender": 2,
- *   "homepage": null,
- *   "id": 15152,
- *   "imdb_id": "nm0000469",
- *   "known_for_department": "Acting",
- *   "name": "James Earl Jones",
- *   "place_of_birth": "Arkabutla, Mississippi, USA",
- *   "popularity": 161.979,
- *   "profile_path": "/sgc8yxYr8ecNn1TXjWXWx3wmUYA.jpg"
- * }
- */
